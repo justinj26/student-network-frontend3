83c18ab1a8965f1890201922a54c30e01288505d
justin_james <jjames11799@gmail.com>
2021-06-22 13:50:11 -0400

    add some dependencies, up to space

--- src/App.js
+++ src/App.js
@@ -1,25 +1,139 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import "./styles.css";
+import Login from "./Login";
+// import Badge from "./Badge";
+import Welcome from "./Welcome";
+import Profile from "./Profile";
+import SignUp from "./SignUp";
+import Home from "./Home";
+import MatchesAndRequests from "./MatchesAndRequests";
+import { Switch, Route } from "react-router-dom";
+import Filter from "./Filter";
+
+// const user = localStorage.getItem("user_id");
+
+// const data = {
+//   college: "University of Michigan",
+//   major: "Computer Science",
+//   extracuriculars: ["Aerospace Engineering", "Dungeons and Dragons"]
+// };
+
+export default function App() {
+  // const data = {
+  //   first_name: "Ike",
+  //   last_name: "Boxton",
+  //   college: "University of Michigan",
+  //   major: "Computer Science",
+  //   extras: ["Aerospace Engineering", "Dungeons and Dragons"]
+  // };
+
+  // some setup, to where if a user is present, go to home page
+  // if not, go to Welcome page
+
+  // const display = (user) ? <Home user={user} > : <Login>
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Home /> */}
+      {/* <Filter /> */}
+      {/* <MatchesAndRequests /> */}
+      <Welcome />
+      {/* {display} */}
+      {/* if (localStorage.getItem("user_id")) */}
+      <Switch>
+        <Route path="/signup">
+          <SignUp />
+        </Route>
+
+        <Route path="/home">
+          {/* user here */}
+          <Home />
+        </Route>
+
+        <Route path="/welcome">
+          <Welcome />
+        </Route>
+
+        <Route path="/profile">
+          <Profile />
+        </Route>
+
+        <Route>{/* <HomePageBadgeGrid users={mentors} /> */}</Route>
+
+        <Route path="/login">
+          <Login />
+        </Route>
+
+        <Route></Route>
+      </Switch>
+      {/* <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br /> */}
+      {/* <SignUp />
+
+      <Welcome />
+      <Login />
+      <Badge user={data} /> */}
     </div>
   );
 }
-
-export default App;
